34237569e21b3acb886898707d0911075f9b24bc
jKALI10 <jerymlop@gmail.com>
2025-03-03 21:31:26 +0300

    new titl

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,26 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SEIFERT Hotel - Addis Ababa, Ethiopia",
+  title: "SEIFERT Hotel",
   description:
     "Where culture meets craft, offering a unique stay with our in-house brews and top-tier amenities in the heart of Ethiopia's vibrant capital, Addis Ababa.",
-    generator: 'v0.dev'
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
 
-
-
-import './globals.css'
+import "./globals.css";
